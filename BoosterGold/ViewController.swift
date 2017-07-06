89ad1991415ffbb83351d73042fe528dc1aaa1b5
Travis Stuever <travisstuever@travis-stuever-macbook-pro.local>
2017-07-05 19:32:42 -0700

    Initial Commit

---
+++ BoosterGold/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoosterGold
+//
+//  Created by Travis Stuever on 7/5/17.
+//  Copyright © 2017 Justice League. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
